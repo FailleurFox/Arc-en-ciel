f846ac206c7e1fd59ad7cd856bfbfa1cdc34601a
Hugo Simond <hugosimond@gmail.com>
2015-09-30 09:37:39 +0200

    Start

---
+++ app/src/androidTest/java/fr/polytech/si5/arc_en_ciel/ApplicationTest.java
@@ -0,0 +1,13 @@
+package fr.polytech.si5.arc_en_ciel;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
